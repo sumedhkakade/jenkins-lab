f80c51d44b87e47019d8f465a97d80cacb0cd1ad
Sumedh Kakde <67014066+sumedhkakade@users.noreply.github.com>
2026-09-19 20:22:54 +0530

    Update app.sh

--- app.sh
+++ app.sh
@@ -4,6 +4,5 @@ echo "Starting Jenkins Lab Application..."
 
 while true
 do
-    echo "Jenkins Lab Application is running..."
-    sleep 30
+    printf 'HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<h1>Jenkins Lab Application</h1><p>Deployed successfully through Jenkins CI/CD!</p>\r\n' | nc -l -p 8080
 done
